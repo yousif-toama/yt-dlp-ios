ab425cfb4f7e0cb335242985807a9c1eba900f9b
Yousif Toama <30576348+yousif-toama@users.noreply.github.com>
2025-10-18 15:12:22 +0100

    Improve shell script

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env dash
+# Set -e is recommended for robust shell scripting
+set -e
+
+# Get the script's absolute directory using $0
+DIR=$(cd "$(dirname "$0")" && pwd)
+cd "$DIR"
+
+# Install required Python packages
 pip install --user --upgrade -r requirements.txt
